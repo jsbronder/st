557f0a1e2e2829c32280f1da2985752970af4617
Justin Bronder <jsbronder@cold-front.org>
2019-05-28 22:05:27 -0400

    config: set colors

--- config.h
+++ config.h
@@ -85,30 +85,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#666666",
+	"#9e1828",
+	"#aece92",
+	"#968a38",
+	"#4E78A0",
+	"#963c59",
+	"#418179",
+	"#bebebe",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#666666",
+	"#cf6171",
+	"#c5f779",
+	"#fff796",
+	"#4186be",
+	"#cf9ebe",
+	"#71bebe",
+	"#ffffff",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
+	"#404040",
 	"#555555",
+	"#262626", // background
+	"#bebebe", //foreground
 };
 
 
@@ -116,8 +118,8 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
