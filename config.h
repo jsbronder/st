693868c15a1253357ed7c9d844d9904e3588e9ff
Justin Bronder <jsbronder@cold-front.org>
2021-09-24 13:34:58 -0400

    config: set shortcuts

--- config.h
+++ config.h
@@ -193,8 +193,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
-    { TERMMOD,              XK_Escape,      keyboard_select,{ 0 } },
+	{ ControlMask,          XK_m,           copyurl,        {.i =  0} },
+	{ ShiftMask,            XK_Escape,      keyboard_select,{.i =  0} },
 };
 
 /*
